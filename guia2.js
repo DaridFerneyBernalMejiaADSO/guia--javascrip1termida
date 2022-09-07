b86ee96710c3a2d3adf499071679898e7b20b3ad
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-07 11:21:19 -0500

    ejercico 6

--- guia2.js
+++ guia2.js
@@ -1,19 +1,7 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4
-    // Longitud de la circunferencia = 2 * PI * radio
-    // Área de la circunferencia = PI * radio˄2
+    let distacia= Number(prompt("ingrese su distacia"));
+    let tiempo= Number(prompt("ingrese el tiempo"));
+    console.log(`la velocidad del proyectil es de ${distacia/tiempo}`)
 
-
-    let radio = Number(prompt("Ingrese el radio de la circunferencia", 5));
-
-
-    console.group(`Formula ${2} * ${Math.PI} * ${radio}`);
-
-        console.log(`Longitud de la circunferencia : "${2 * Math.PI * radio}"`);
-    console.groupEnd();
-
-    console.group(`Formula ${Math.PI} * ${Math.pow(radio, 2)}`);
-        console.log(`Área de la circunferencia : "${Math.PI * Math.pow(radio, 2)}"`);
-    console.groupEnd();
 })
     
