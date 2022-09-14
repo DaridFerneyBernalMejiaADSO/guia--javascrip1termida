3477d8b76264385a0da9dd57ab98a47121b4203c
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-14 10:12:57 -0500

    ejercicio 21

--- guia2.js
+++ guia2.js
@@ -1,14 +1,7 @@
 addEventListener("DOMContentLoaded", ()=>{
-   // Un vendedor recibe un sueldo base más el 10% de comisión sobre sus ventas. Si en un mes
-   // cualquiera hace tres ventas por valores: v1, v2 y v3, ¿cuánto recibirá por comisión? y ¿cuánto en total
-   // sueldo del vendedor?.
-   // comision y sueldo del vendedor
-    let smensual= Number(prompt(`ingrese su sueldo mensual`))
-    let v1= Number(prompt(`ingrese el valor de la venta 1`))
-    let v2= Number(prompt(`ingrese el valor de la venta 2`))
-    let v3= Number(prompt(`ingrese el valor de la venta 3`))
-    let ventatotal=v1+v2+v3
-    let comision=ventatotal*0.10
-    console.log(`la comision que saco este mes es de=${comision}`)
-    console.log(`el sueldo es de ${smensual+comision}`)
+   // calcular la distancia entre dos puntos
+   let x= Number(prompt(`ingrese el primer punto `))
+   let y=Number(prompt(`ingrese el segundo punto`))
+   let d=rc*(((x*2)-(x*1))**2 + ((y*2)-(y*1))**2)
+   console.log(`la distancia que hay entre dos puntos es de = ${d}`)
 })
