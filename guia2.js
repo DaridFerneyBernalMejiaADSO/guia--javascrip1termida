0aeac5f9924c972c4b6384f9ba3671b66fee6aba
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-14 09:03:34 -0500

    ejercicio 17

--- guia2.js
+++ guia2.js
@@ -1,20 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-  //  calcular el promedio final
- // . Elaborar un algoritmo para calcular el promedio final de la materia de algoritmos. Dicha calificación se
-// compone de los siguientes porcentajes:
-// • 55% del promedio final de sus calificaciones de los tres(3) parciales.
-// • 30% de la calificación examen final y
-// • 15% de la calificación trabajo final.
-let p1 =Number(prompt(`ingrese la nota del parcial 1`))
-let p2 =Number(prompt(`ingrese la nota del parcial 2`))
-let p3 =Number(prompt(`ingrese la nota del parcial 3`))
-let ef =Number(prompt(`ingrese la calificacion del examen final`))
-let tf =Number(prompt(`ingrese la calificacion del  trabajo final`))
-let calificaciones=(p1+p2+p3)/3
-let ptcalificaciones=calificaciones*0.55
-let examenf=ef*0.30
-let trabajof=tf*0.15
-let promediofinal=ptcalificaciones+examenf+trabajof
-console.log(`el promedio final es= ${promediofinal}`)
+   // Dado el valor que un cliente paga por un producto, calcular qué valor corresponde al costo total del
+   // producto y cuánto es el valor del IVA. Considerando que el porcentaje del IVA puede variar en el
+   // tiempo y de un producto a otro, este dato se lee por teclado.
+   // el prducto y el iva precio 
+   let productop=Number(prompt(`ingrese el precio del producto`))
+   iva=productop*0.20
+console.log(`el iva en el producto es de=${iva}`)
+console.log(`el valor total a pagar es de=${productop+iva}`)
 })
     
