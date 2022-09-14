10190bdd82250a68b287d3a006c912941cbd1e8c
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-14 07:52:14 -0500

    ejercicio 9

--- guia2.js
+++ guia2.js
@@ -1,9 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-   //calculo de una expresion
-   let a=8
-   let b=12
-   let c=18
-   let rta=(a+7*c)/(b+2-a)+2*b 
-   console.log(`la respuesta es= ${rta}`)
+//# el área y el volumen de un cilindro
+let h= 12
+let r= 18
+let pi= Math.PI
+let area= (2*(pi*r**2))+((2*pi*r)*h)
+let volumen=(pi*r**2)*h
+console.log(`el area del cilindro es:${area}`)
+console.log(`el volumen de un cilindro es:${volumen}`)
+
 })
     
