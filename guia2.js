56dfddb5d8d78f4de0536f197cfd00da647e3a11
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-14 08:47:26 -0500

    ejercicio 15

--- guia2.js
+++ guia2.js
@@ -1,8 +1,7 @@
 addEventListener("DOMContentLoaded", ()=>{
-//Una farmacia
-let preciop= Number(prompt(`precio del producto`))
-let descuento=preciop*0.10
-console.log(`el descuento del producto es ${descuento}`)
-console.log(`el valor a pagar es ${preciop-descuento}`)
+  // grados centígrados a grados Fahrenheit
+let g= Number(prompt(`ingrese sus grados centigrados `))
+let f=(g*1.8)+32
+console.log(`el resultado es ${f}`)
 })
     
